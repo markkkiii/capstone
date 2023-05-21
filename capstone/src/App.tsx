026ef31839ca37901a429f03bc25066ba760ecd4
Art <honeydewart@gmail.com>
2023-05-21 21:57:00 +0800

    update
    new npm

---
+++ capstone/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <b>Learn How to Settle for One!</b>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
