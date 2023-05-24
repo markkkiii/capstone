56db784517c2de9e77565c4a735b846893724983
Art <honeydewart@gmail.com>
2023-05-24 15:25:35 +0800

    Update App.tsx

--- capstone/src/App.tsx
+++ capstone/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <b>Learn How to Settle for One!</b>
+          <b>Sergio moang!</b>
         </p>
       </header>
     </div>
