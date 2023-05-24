cf1c7769b858a0080915ecf6be07bfdc3f6d8b2d
SERGIO NUNEZ <sergio.nunezjr@cit.edu>
2023-05-24 15:27:10 +0800

    TESTT
    TEST

--- capstone/src/App.tsx
+++ capstone/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <b>Sergio moang!</b>
+          <b>TESTT</b>
         </p>
       </header>
     </div>
