350adda26a106e56a531b29fd3895ce27677f3b3
Art <honeydewart@gmail.com>
2023-05-25 20:56:24 +0800

    Update App.tsx

--- capstone/src/App.tsx
+++ capstone/src/App.tsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faCalendarAlt, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import BuildingList from './BuildingList';
-import ViewPending from './ViewPending';
 
 
 export default function App(){
