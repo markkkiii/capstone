e935cfe92e20ddd98ca5614be1e516928762d7e6
Art <honeydewart@gmail.com>
2023-05-26 16:59:48 +0800

    update

--- capstone/src/App.tsx
+++ capstone/src/App.tsx
@@ -8,7 +8,7 @@ export default function App(){
   return(
     <BrowserRouter>
         <Routes>
-          <Route path = "/" element={<BuildingList/>}/>
+          <Route path = "/dashboard" element={<BuildingList/>}/>
         </Routes>
     </BrowserRouter>
 
