72cc19f0905e3962a7fb4e5e0834057fc8750338
SERGIO NUNEZ <sergio.nunezjr@cit.edu>
2023-05-30 11:09:35 +0800

    updatefunc
    updatefunc

--- capstone/src/UpdateApplicationPopUp.tsx
+++ capstone/src/UpdateApplicationPopUp.tsx
@@ -6,7 +6,16 @@ import DialogActions from '@mui/material/DialogActions';
 import CancelIcon from '@mui/icons-material/Cancel';
 import AddApplicationForm from './AddApplicationForm';
 import IconButton from '@mui/material/IconButton';
-import { DialogTitle } from '@mui/material';
+import { Card, CardContent, DialogTitle, Grid, OutlinedInput, Stack } from '@mui/material';
+
+
+const cardStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  maxWidth: 800,
+  backgroundColor: 'lightgrey'
+}; //Style Purposes
 
 
 
@@ -36,12 +45,63 @@ const UpdateApplicationPopup: React.FC<formdetails> = ({no, buildingPermitNo, ap
                     </IconButton>
                 </DialogTitle>
                 <DialogContent sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} style={{ paddingTop: '2rem' }} >
-                    <AddApplicationForm
-                    no={no}
-                    buildingPermitNo={buildingPermitNo}
-                    applicantName={applicantName}
-                    projectName={projectName}
-                    />
+                <>
+                    <Card style= {cardStyle}>
+                        <CardContent style={{ marginLeft: 35, textAlign:'center' }} >
+                        <Grid container >
+                            <Grid item xs={10} sm={11}>
+                            <Stack spacing={-1} sx={{alignItems:'flex-start'}}>
+                                <p className='custom-paragraph'>Building Permit Number</p>
+                                <OutlinedInput fullWidth className='custom-outlined-input' sx={{borderRadius: '11px'}} defaultValue={props.buildingPermitNo}/>
+                            </Stack>
+                            </Grid>
+                            <Grid item xs={10} sm={11}>
+                            <Stack spacing={-1} sx={{alignItems:'flex-start'}}>
+                                <p className='custom-paragraph'>Name of Owner/Permitee</p>
+                                <OutlinedInput fullWidth className='custom-outlined-input' sx={{borderRadius: '11px'}} defaultValue={props.applicantName}/>
+                            </Stack>
+                            </Grid>
+                            <Grid item xs={10} sm={11}>
+                            <Stack spacing={-1} sx={{alignItems:'flex-start'}}>
+                                <p className='custom-paragraph'>Business Name</p>
+                                <OutlinedInput fullWidth className='custom-outlined-input' sx={{borderRadius: '11px'}} defaultValue={props.projectName}/>
+                            </Stack>
+                            </Grid>
+                            <Grid item xs={10} sm={11}>
+                            <Stack spacing={-1} sx={{alignItems:'flex-start'}}>
+                                <p className='custom-paragraph'>Address</p>
+                                <OutlinedInput fullWidth className='custom-outlined-input' sx={{borderRadius: '11px'}}/>
+                            </Stack>
+                            </Grid>
+                            <Grid item xs={10} sm={6}>
+                            <Stack spacing={-1} sx={{alignItems:'flex-start'}}>
+                                <p className='custom-paragraph'>Type of Occupancy</p>
+                                <OutlinedInput fullWidth className='custom-outlined-input' sx={{borderRadius: '11px'}}/>
+                            </Stack>
+                            </Grid>
+                            <Grid item xs={10} sm={5}>
+                            <Stack spacing={-1} sx={{alignItems:'flex-start'}}>
+                                <p className='custom-paragraph'>Contact Number</p>
+                                <OutlinedInput fullWidth className='custom-outlined-input' sx={{borderRadius: '11px'}}/>
+                            </Stack>
+                            </Grid>
+                            <Grid item xs={10} sm={5}>
+                            <Stack spacing={-1} sx={{alignItems:'flex-start'}}>
+                                <p className='custom-paragraph'>Date Received</p>
+                                <OutlinedInput fullWidth className='custom-outlined-input' sx={{borderRadius: '11px'}} defaultValue={selectedDate} onChange={handleDateChange}/>
+                            </Stack>
+                            </Grid>
+                            <Grid item xs={10} sm={6}>
+                            <Stack spacing={-1} sx={{alignItems:'flex-start'}}>
+                                <p className='custom-paragraph'>Received By</p>
+                                <OutlinedInput fullWidth className='custom-outlined-input' sx={{borderRadius: '11px'}}/>
+                            </Stack>
+                            </Grid>
+                        </Grid>
+                        </CardContent>
+                    </Card>
+
+    </>
                 </DialogContent>
                 <DialogActions style={{ justifyContent: 'center' }}>
                     <Button variant='contained' onClick={handleClose} sx={{backgroundColor:'grey', borderRadius:'13px', height:'30px'}}>Update Application</Button>
