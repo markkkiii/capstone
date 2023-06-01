36b690cd879d3ed1095345030310b80b58eb1067
Art <honeydewart@gmail.com>
2023-06-01 11:19:55 +0800

    update everything

---
+++ capstone/src/DeletePopup.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import axios from 'axios';
+
+interface DeleteProps {
+    value: number;
+    open: boolean;
+    handleClose: () => void;
+}
+
+const DeletePopup: React.FC<DeleteProps> = ({ value, open, handleClose }) => {
+
+    const deletefunc = (value: number) => {
+        //function here
+        axios.delete('http://localhost:8080/BFP/deletePermit/' + value).then(res => {
+            console.log(res.data);
+            alert("Deleted Successfully!");
+            handleClose()
+        }).catch(err => console.log(err))
+    }
+
+    return (
+        <div>
+            <Dialog open={open} maxWidth="xs" fullWidth PaperProps={{ style: { backgroundColor: 'lightgrey' } }} >
+                <DialogContent sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <p style={{ fontFamily: 'Oswald', fontWeight: 'bold' }}>Do you want to delete this form?</p>
+                </DialogContent>
+                <DialogActions style={{ justifyContent: 'center' }}>
+                    <Button variant='contained' onClick={() => deletefunc(value)} sx={{ backgroundColor: '#BEBEBE', fontFamily: 'Oswald', color: 'black' }}>Delete</Button>
+                    <Button variant='contained' onClick={handleClose} sx={{ backgroundColor: '#BEBEBE', fontFamily: 'Oswald', color: 'black' }}>Cancel</Button>
+                </DialogActions>
+            </Dialog>
+
+        </div>
+    );
+};
+
+export default DeletePopup;
